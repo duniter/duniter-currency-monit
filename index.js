31cfee42a3040a03585d0c59dc54187610fdaabe
librelois <elois@ifee.fr>
2017-04-26 16:00:44 +0200

    fix #10

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const duniter = require('duniter');
  ***************************************/
 
 // Default Duniter node's database
-const HOME_DUNITER_DATA_FOLDER = 'currency-monit-clean-dev';
+const DEFAULT_DUNITER_DATA_FOLDER = 'currency-monit-dev';
 
 // host on which UI is available
-const SERVER_HOST = 'localhost';
+const DEFAULT_HOST = 'localhost';
 
 // port on which UI is available
-const SERVER_PORT = 10503;
+const DEFAULT_PORT = 10500;
 
 /****************************************
  * SPECIALIZATION
@@ -28,13 +28,17 @@ const stack = duniter.statics.autoStack([{
     duniter: {
 
       cli: [{
-        name: 'currency-monit [host] [port]',
+        name: 'currency-monit [host] [port] [data-folder]',
         desc: 'Starts specialized node currency-monit',
 
         // Disables Duniter node's logs
         logs: false,
 
         onDatabaseExecute: (server, conf, program, params, startServices) => co(function*() {
+	  
+	  // currency-monit parameters
+          const SERVER_HOST = params[0] || DEFAULT_HOST;
+          const SERVER_PORT = parseInt(params[1]) || DEFAULT_PORT;
 
           // IMPORTANT: release Duniter services from "sleep" mode
           yield startServices();
@@ -54,7 +58,7 @@ co(function*() {
   if (!process.argv.includes('--mdb')) {
     // We use the default database
     process.argv.push('--mdb');
-    process.argv.push(HOME_DUNITER_DATA_FOLDER);
+    process.argv.push(DEFAULT_DUNITER_DATA_FOLDER);
   }
   // Execute our program
   yield stack.executeStack(process.argv);
