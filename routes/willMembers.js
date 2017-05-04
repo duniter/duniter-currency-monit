314bfeee76eb79f68a3ea0081bec43176d6e0301
librelois <elois@ifee.fr>
2017-05-04 04:45:00 +0200

    [willMembers] remove cache results, it complicates enormously the code, generates many bug, and does not offer gain of performance

--- routes/willMembers.js
+++ routes/willMembers.js
@@ -1,16 +1,10 @@
 "use strict";
 
 const co = require('co')
+const crypto = require('crypto');
 const timestampToDatetime = require('../lib/timestampToDatetime')
 
-// informatiosn a preserver entre 2 chargements de la page
-var previousBlockNumber = 0;
-var previousBlockHash = "";
-var previousBlochainTimestamp = 0;
-var identitiesList = [];
-var idtysPendingCertifsList = [ [ [] ] ];
-var nbMaxCertifs = 0;
-var countMembersWithSigQtyValidCert = 0;
+// Préserver les résultats en cache
 
 module.exports = (req, res, next) => co(function *() {
   
@@ -25,8 +19,13 @@ module.exports = (req, res, next) => co(function *() {
     
     // Initaliser les variables
     var errors = "";
-    //var identitiesList = [];
-    //var idtysPendingCertifsList = [ [ [] ] ];
+    var identitiesList = [];
+    var idtysPendingCertifsList = [ [ [] ] ];
+    var previousBlockNumber = 0;
+    var previousBlockHash = "";
+    var previousBlochainTimestamp = 0;
+    var nbMaxCertifs = 0;
+    var countMembersWithSigQtyValidCert = 0;
     
     // Récupérer les paramètres
     var days = req.query.d || 65 // Valeur par défaut
@@ -35,24 +34,6 @@ module.exports = (req, res, next) => co(function *() {
     var hideIdtyWithZeroCert = req.query.hideIdtyWithZeroCert || "no"; // Valeur par défaut
     var sortSig = req.query.sortSig || "Availability"; // Valeur par défaut
     var format = req.query.format || 'HTML'
-    
-    // Si idtysPendingCertifsList est déjà rempli, vérifier si nous sommes toujours sur la même branche
-    if (idtysPendingCertifsList.length > 0)
-    {
-      // Récupérer le hash du précédent bloc courant 
-      let resultQueryPreviousBlock = yield duniterServer.dal.peerDAL.query('SELECT `hash` FROM block WHERE `number`='+previousBlockNumber+' LIMIT 1 ');
-      
-      // Si nous ne sommes plus sur la même branche, tout réinitialiser
-      if (resultQueryPreviousBlock[0].hash != previousBlockHash)
-      {
-	previousBlockNumber=0;
-	previousBlockHash = "";
-	previousBlochainTimestamp = 0;
-	identitiesList.splice(0,identitiesList.length);
-	idtysPendingCertifsList.splice(0,idtysPendingCertifsList.length);
-	nbMaxCertifs = 0;
-      }
-    }
     
     // Récupérer la liste des identités en piscine
     const resultQueryIdtys = yield duniterServer.dal.peerDAL.query('SELECT `buid`,`pubkey`,`uid`,`hash`,`expires_on` FROM identities_pending WHERE `member`=0');
@@ -66,72 +47,53 @@ module.exports = (req, res, next) => co(function *() {
       
       // récupérer le medianTime du bloc d'émission de l'identité
       let resultQueryTimeCreateIdty = yield duniterServer.dal.peerDAL.query('SELECT `medianTime` FROM block WHERE `number`=\''+idtyBlockNumber+'\' LIMIT 1');
-	
-      // N'ajouter l'identité que si elle à été émise après le dernier remplissage de idtysPendingCertifsList
-      if (resultQueryTimeCreateIdty[0].medianTime > previousBlochainTimestamp)
-      {
+
 	// Stocker les informations de l'identité
 	identitiesList.push({
-	  BlockNumber: idtyBlockNumber,
-	  creationTimestamp: resultQueryTimeCreateIdty[0].medianTime,
-	  pubkey: resultQueryIdtys[i].pubkey,
-	  uid: resultQueryIdtys[i].uid,
-	  hash: resultQueryIdtys[i].hash,
-	  expires_on: resultQueryIdtys[i].expires_on,
-	  nbValidCert: 0,
-	  registrationAvailability: 0
+	    BlockNumber: idtyBlockNumber,
+	    creationTimestamp: resultQueryTimeCreateIdty[0].medianTime,
+	    pubkey: resultQueryIdtys[i].pubkey,
+	    uid: resultQueryIdtys[i].uid,
+	    hash: resultQueryIdtys[i].hash,
+	    expires_on: resultQueryIdtys[i].expires_on,
+	    nbCert: 0,
+	    nbValidPendingCert: 0,
+	    registrationAvailability: 0
 	});
 	idtysPendingCertifsList.push(new Array());
-      }
       
       // récupérer l'ensemble des certifications en attente destinées à l'identité courante
       let tmpQueryPendingCertifsList = yield duniterServer.dal.peerDAL.query(
 	'SELECT `from`,`block_number`,`block_hash`,`expires_on` FROM certifications_pending WHERE `to`=\''+resultQueryIdtys[i].pubkey+'\' AND `target`=\''+resultQueryIdtys[i].hash+'\' ORDER BY `expires_on` DESC');
-	
-      // Calculer le nombre de certifications en attentes destinées au membre courant
-      let nbPendingCertifs = tmpQueryPendingCertifsList.length;
-      let nbValidPendingCertifs = 0;
-      
-      // Trouver l'identité correspondante dnas le tableau identitiesList
-      let indexIdty = -1;
-      for (let i2=0;i2<identitiesList.length;i2++)
-      {
-	if (identitiesList[i2].hash == resultQueryIdtys[i].hash)
-	{ indexIdty = i2; }
-      }
-      if (indexIdty == -1) { console.log("FATAL ERROR : idty %s (buid %s) not find in identitiesList memory !", resultQueryIdtys[i].uid, resultQueryIdtys[i].buid) }
 	  
       // Récupérer les uid des émetteurs des certifications reçus par l'utilisateur
       // Et stocker les uid et dates d'expiration dans un tableau
-      
-      for (let j=0;j<nbPendingCertifs;j++)
+      for (let j=0;j<tmpQueryPendingCertifsList.length;j++)
       {
-	// ne traiter la certif courante que si elle a été émise après le dernier remplissage de idtysPendingCertifsList
-	if (tmpQueryPendingCertifsList[j].block_number > previousBlockNumber)
-        {
-	  let tmpQueryGetUidIssuerPendingCert = yield duniterServer.dal.peerDAL.query('SELECT `uid` FROM i_index WHERE `pub`=\''+tmpQueryPendingCertifsList[j].from+'\' LIMIT 1');
+        // Récupérer le medianTime et le hash du bloc d'émission de la certification 
+	let emittedBlock = yield duniterServer.dal.peerDAL.query('SELECT `hash`,`medianTime` FROM block WHERE `number`=\''+tmpQueryPendingCertifsList[j].block_number+'\' AND `fork`=0 LIMIT 1');
+	  
 	  // Vérifier que l'émetteur de la certification correspond à une identité inscrite en blockchain
+	  let tmpQueryGetUidIssuerPendingCert = yield duniterServer.dal.peerDAL.query('SELECT `uid` FROM i_index WHERE `pub`=\''+tmpQueryPendingCertifsList[j].from+'\' LIMIT 1');
 	  if ( tmpQueryGetUidIssuerPendingCert.length > 0 )
 	  {
+	    // Vérifier si le blockstamp est correct
+	    var validBlockStamp = false;
+	    if (emittedBlock[0].hash == tmpQueryPendingCertifsList[j].block_hash)
+	    { validBlockStamp = true; }
+	    
 	    // récupérer le timestamp d'enchainement de la dernière certification écrite par l'émetteur
 	    let tmpQueryLastIssuerCert = yield duniterServer.dal.peerDAL.query('SELECT `chainable_on` FROM c_index WHERE `issuer`=\''+tmpQueryPendingCertifsList[j].from+'\' ORDER BY `chainable_on` DESC LIMIT 1');
 	    let certTimestampWritable = 0;
 	    if ( typeof(tmpQueryLastIssuerCert[0]) != 'undefined' && typeof(tmpQueryLastIssuerCert[0].chainable_on) != 'undefined' )
 	    { certTimestampWritable = tmpQueryLastIssuerCert[0].chainable_on; }
-	    
-	    // Vérifier si le blockstamp est correct
-	    var validBlockStamp = false;
-	    let tmpBlockHash = yield duniterServer.dal.peerDAL.query('SELECT `hash` FROM block WHERE `number`=\''+tmpQueryPendingCertifsList[j].block_number+'\' AND `fork`=0 LIMIT 1');
-	    if (tmpBlockHash[0].hash == tmpQueryPendingCertifsList[j].block_hash)
-	    {
-	      validBlockStamp = true;
-	    }
+	    identitiesList[i].registrationAvailability = (certTimestampWritable > identitiesList[i].registrationAvailability) ? certTimestampWritable : identitiesList[i].registrationAvailability;
 	    
 	    // Vérifier que l'identité courant n'a pas déjà reçu d'autre(s) certification(s) de la part du même membre ET dans le même état de validité du blockstamp
 	    let doubloonPendingCertif = false;
-	    for (const idtyPendingCertifsList of idtysPendingCertifsList[indexIdty])
+	    for (const pendingCert of idtysPendingCertifsList[i])
 	    {
-	      if (idtyPendingCertifsList.from == tmpQueryGetUidIssuerPendingCert[0].uid && idtyPendingCertifsList.validBlockStamp == validBlockStamp)
+	      if (pendingCert.from == tmpQueryGetUidIssuerPendingCert[0].uid && pendingCert.validBlockStamp == validBlockStamp)
 	      {
 		doubloonPendingCertif = true;
 	      }
@@ -141,22 +103,26 @@ module.exports = (req, res, next) => co(function *() {
 	      // Stoker la liste des certifications en piscine qui n'ont pas encore expirées
 	      if (tmpQueryPendingCertifsList[j].expires_on > currentBlockchainTimestamp)
 	      {
-		idtysPendingCertifsList[indexIdty].push({
+		idtysPendingCertifsList[i].push({
 		  from: tmpQueryGetUidIssuerPendingCert[0].uid,
+		  pubkey: tmpQueryPendingCertifsList[j].from,
 		  blockNumber: tmpQueryPendingCertifsList[j].block_number,
 		  timestampExpire: tmpQueryPendingCertifsList[j].expires_on,
 		  timestampWritable: certTimestampWritable,
 		  validBlockStamp: validBlockStamp
 		});
-		nbValidPendingCertifs++;
+		identitiesList[i].nbCert++;
+		if (validBlockStamp) { identitiesList[i].nbValidPendingCert++; }
 	      }
 	    }
 	  }
-        }
       }
-	
+	  
       // Calculer le nombre maximal de certifications reçues par l'identité courante
-      if ( nbValidPendingCertifs > nbMaxCertifs) { nbMaxCertifs = nbValidPendingCertifs; }
+      if ( identitiesList[i].nbCert > nbMaxCertifs) { nbMaxCertifs = identitiesList[i].nbCert; }
+      
+      // calculate countMembersWithSigQtyValidCert
+      if ( identitiesList[i].nbValidPendingCert >= sigQty) { countMembersWithSigQtyValidCert++; }
     }
       
     // Si demandé, retrier les, certifications par date de disponibilité
@@ -211,38 +177,27 @@ module.exports = (req, res, next) => co(function *() {
     if (sort_by == "creationIdty")
     { 
       for (const idty of identitiesList)
-      { tabSort.push(idty.expires_on); }
+      {
+	tabSort.push(idty.expires_on);
+      }
     }
     else if (sort_by == "sigCount" || sort_by == "registrationPackage")
     {
         // idtys loop
-	for (let i=0;i<idtysPendingCertifsList.length;i++)
+	for (const idty of identitiesList)
         {
-	  let nbValidCert = 0;
-	  let registrationAvailability= 0;
-	  // Count nbValidCert and calculate registrationAvailability timestamp
-	  for (const cert of idtysPendingCertifsList[i])
-	  {
-	    if (cert.validBlockStamp)
-	    {
-	      nbValidCert++;
-	      registrationAvailability = (cert.timestampWritable > registrationAvailability) ? cert.timestampWritable : registrationAvailability;
-	    }
-	  }
-	  identitiesList[i].nbValidCert = nbValidCert;
-	  identitiesList[i].registrationAvailability = registrationAvailability;
-	  
 	  // Calculate registrationAvailabilityDelay
-	  let registrationAvailabilityDelay = (registrationAvailability > currentBlockchainTimestamp) ? (registrationAvailability-currentBlockchainTimestamp):0;
+	  let registrationAvailabilityDelay = (idty.registrationAvailability > currentBlockchainTimestamp) ? (idty.registrationAvailability-currentBlockchainTimestamp):0;
 	  
 	  // Trier les identités au dossier complet par durée entre date de disponibilité et date d'expiration maximale théorique (=sigWindow-registrationAvailabilityDelay)
-	  if (sort_by == "registrationPackage" && nbValidCert > sigQty)
+	  // Attribuer un malus de sigValidity secondes par certification valide (plafonner à sigQty dans le cas de 'registrationPackage') 
+	  if (sort_by == "registrationPackage" && idty.nbValidPendingCert > sigQty)
 	  {
 	    tabSort.push(sigWindow-registrationAvailabilityDelay + (sigValidity*sigQty));
 	  }
 	  else
 	  {
-	    tabSort.push(sigWindow-registrationAvailabilityDelay + (sigValidity*nbValidCert));
+	    tabSort.push(sigWindow-registrationAvailabilityDelay + (sigValidity*idty.nbValidPendingCert));
 	  }
         }
     }
@@ -251,31 +206,6 @@ module.exports = (req, res, next) => co(function *() {
     // Trier les identités par ordre decroissant du critère sort_by
     for (var i=0;i<identitiesList.length;i++)
     {
-      // Vérifier que l'identité n'est pas devenue membre et na pas expiréée
-      let continue_ = true;
-      while (continue_)
-      {
-	  let tmpCheckIdty = yield duniterServer.dal.peerDAL.query('SELECT `uid` FROM i_index WHERE `member`=1 AND `pub`=\''+identitiesList[i].pubkey+'\' LIMIT 1');
-	  // Si l'on trouve une identité membre correspondante ou que l'identité a expirée, alors supprimer cette identité des tableaux identitiesList et idtysPendingCertifsList
-	  if (tmpCheckIdty.length > 0 || identitiesList[i].expires_on <= currentBlockchainTimestamp)
-	  {
-	    // Décrementer countMembersWithSigQtyValidCert
-	    if (identitiesList[i].nbValidCert > sigQty) { countMembersWithSigQtyValidCert--; }
-	    
-	    // Supprimer l'identité et toutes les certifications qu'elle a reçue
-	    identitiesList.splice(i, 1);
-	    idtysPendingCertifsList.splice(i, 1);
-	    
-	    // Arrèter la boucle si le tableau est devenu vide
-	    if (identitiesList.length == 0) { continue_ = false; }
-	  }
-	  // Sinon, c'est que l'on a trouver une identité qui n'est pas membre, on sort de cette boucle et on la traite
-	  else { continue_ = false; }
-      }
-      
-      // calculate countMembersWithSigQtyValidCert
-      if ( identitiesList[i].nbValidCert >= sigQty && identitiesList[i].creationTimestamp > previousBlochainTimestamp) { countMembersWithSigQtyValidCert++; }
-      
       let max = -1;
       let idMax =0;
       for (var j=0;j<identitiesList.length;j++)
@@ -307,7 +237,7 @@ module.exports = (req, res, next) => co(function *() {
           pubkey: identitiesList[idMax].pubkey,
           BlockNumber: identitiesList[idMax].BlockNumber,
           expires_on: identitiesList[idMax].expires_on,
-          nbValidCert: identitiesList[idMax].nbValidCert,
+          nbValidPendingCert: identitiesList[idMax].nbValidPendingCert,
           pendingCertifications: idtysPendingCertifsList[idMax]
           });
         }
