38b6fb25ad6e9621e359aab3e46cf140150acc29
librelois <elois@ifee.fr>
2017-05-13 03:13:36 +0200

    [membersCount] add sentries count and traductions (FR, EN)

--- routes/membersCount.js
+++ routes/membersCount.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const co = require('co')
+//var math = require('mathjs');
 const timestampToDatetime = require('../lib/timestampToDatetime')
 const getLang = require('../lib/getLang')
 
@@ -23,11 +24,11 @@ module.exports = (req, res, next) => co(function *() {
     // get blockchain
     if (end >= begin && begin >= 0)
     {
-      var blockchain = yield duniterServer.dal.peerDAL.query('SELECT `issuer`,`membersCount`,`medianTime`,`dividend`,`number`,`nonce`,`issuersCount` FROM block WHERE `fork`=0 AND `medianTime` <= '+endBlock[0].medianTime+' AND `medianTime` >= '+beginBlock[0].medianTime+' ORDER BY `medianTime` ASC');
+      var blockchain = yield duniterServer.dal.peerDAL.query('SELECT `hash`,`issuer`,`membersCount`,`medianTime`,`dividend`,`number`,`nonce`,`issuersCount` FROM block WHERE `fork`=0 AND `medianTime` <= '+endBlock[0].medianTime+' AND `medianTime` >= '+beginBlock[0].medianTime+' ORDER BY `medianTime` ASC');
     }
     else
     {
-      var blockchain = yield duniterServer.dal.peerDAL.query('SELECT `issuer`,`membersCount`,`medianTime`,`dividend`,`number`,`nonce`,`issuersCount` FROM block WHERE `fork`=0 AND `medianTime` >= '+beginBlock[0].medianTime+' ORDER BY `medianTime` ASC');
+      var blockchain = yield duniterServer.dal.peerDAL.query('SELECT `hash`,`issuer`,`membersCount`,`medianTime`,`dividend`,`number`,`nonce`,`issuersCount` FROM block WHERE `fork`=0 AND `medianTime` >= '+beginBlock[0].medianTime+' ORDER BY `medianTime` ASC');
     }
     
     // Get blockchain timestamp
@@ -51,27 +52,59 @@ module.exports = (req, res, next) => co(function *() {
     let stepIssuerCount = 0;
     let bStep = 0;
     
-    // Create and fill tabMembersCount
+    // Create and fill members and tabMembersCount
+    var members = [];
     var tabMembersCount = [];
     for (let b=0;b<blockchain.length;b++)
     {
       stepIssuerCount += blockchain[b].issuersCount;
       bStep++;
+      
+      // push new members
+      if (blockchain[b].membersCount > members.length)
+      {
+	let newMembers = yield duniterServer.dal.peerDAL.query('SELECT `pub` FROM i_index WHERE `written_on`=\''+blockchain[b].number+'-'+blockchain[b].hash+'\'');
+	for (let nm=0;nm<newMembers.length;nm++) { members.push(newMembers[nm].pub); }
+      }
+      
+      // If achieve next step
       if (blockchain[b].medianTime >= nextStepTime)
       {
-	  tabMembersCount.push({
+	// count sentries
+	let Yn = Math.ceil(Math.pow(blockchain[b].membersCount, 1/5));
+	let sentriesCount = 0;
+	for (let m=0;m<members.length;m++)
+	{
+	  let writtenCerts = yield duniterServer.dal.peerDAL.query('SELECT `op` FROM c_index WHERE `issuer`=\''+members[m]+'\'');
+	  let receivedCerts = yield duniterServer.dal.peerDAL.query('SELECT `op` FROM c_index WHERE `receiver`=\''+members[m]+'\'');
+	  if (writtenCerts.length >= Yn && receivedCerts.length >= Yn) { sentriesCount++; }
+	}
+	
+	// push tabMembersCount
+	tabMembersCount.push({
 	    blockNumber: blockchain[b].number,
 	    timestamp: blockchain[b].medianTime,
 	    dateTime: timestampToDatetime(blockchain[b].medianTime),
 	    membersCount: blockchain[b].membersCount,
+	    sentriesCount: sentriesCount,
 	    issuersCount: parseInt(stepIssuerCount/bStep)
-	  });
+	});
 	  
-	  nextStepTime += stepTime;
-	  stepIssuerCount = 0;
-	  bStep = 0;
+	nextStepTime += stepTime;
+	stepIssuerCount = 0;
+	bStep = 0;
       }
     }
+    
+    // count sentries at current block
+    let Yn = Math.ceil(Math.pow(blockchain[blockchain.length-1].membersCount, 1/5));
+	let sentriesCount = 0;
+	for (let m=0;m<members.length;m++)
+	{
+	  let writtenCerts = yield duniterServer.dal.peerDAL.query('SELECT `op` FROM c_index WHERE `issuer`=\''+members[m]+'\'');
+	  let receivedCerts = yield duniterServer.dal.peerDAL.query('SELECT `op` FROM c_index WHERE `receiver`=\''+members[m]+'\'');
+	  if (writtenCerts.length >= Yn && receivedCerts.length >= Yn) { sentriesCount++; }
+	}
     
     // Add current block data
     tabMembersCount.push({
@@ -79,6 +112,7 @@ module.exports = (req, res, next) => co(function *() {
 	    timestamp: blockchain[blockchain.length-1].medianTime,
 	    dateTime: timestampToDatetime(blockchain[blockchain.length-1].medianTime),
 	    membersCount: blockchain[blockchain.length-1].membersCount,
+	    sentriesCount: sentriesCount,
 	    issuersCount: blockchain[blockchain.length-1].issuersCount
 	  });
     
@@ -87,7 +121,7 @@ module.exports = (req, res, next) => co(function *() {
     else
     {
       // get lg file
-      const LANG = getLang(`./lg/pubkeyBalance_${req.query.lg||'fr'}.txt`);
+      const LANG = getLang(`./lg/membersCount_${req.query.lg||'fr'}.txt`);
       
       // GET parameters
       var unit = req.query.unit == 'relative' ? 'relative' : 'quantitative';
@@ -98,7 +132,7 @@ module.exports = (req, res, next) => co(function *() {
         tabMembersCount,
         begin, 
         end,
-        form: `Begin #<input type="number" name="begin" value="${begin}" min="0"> - End #<input type="number" name="end" value="${end}" min="1"> - step <input type="number" name="step" value="${step}" min="1">
+        form: `${LANG["BEGIN"]} #<input type="number" name="begin" value="${begin}" min="0"> - ${LANG["END"]} #<input type="number" name="end" value="${end}" min="1"> - ${LANG["STEP"]} <input type="number" name="step" value="${step}" min="1">
 	  <select name="stepUnit">
 	      <option name="stepUnit" value ="hours"${stepUnit == 'hours' ? 'selected' : ''}>${LANG["HOURS"]}
 	      <option name="stepUnit" value ="days" ${stepUnit == 'days' ? 'selected' : ''}>${LANG["DAYS"]}
@@ -106,13 +140,13 @@ module.exports = (req, res, next) => co(function *() {
 	      <option name="stepUnit" value ="months" ${stepUnit == 'months' ? 'selected' : ''}>${LANG["MONTHS"]}
 	      <option name="stepUnit" value ="years" ${stepUnit == 'years' ? 'selected' : ''}>${LANG["YEARS"]}
 	    </select>`,
-	description: ``,
+	description: `${LANG["DESCRIPTION1"]+'<br>'+LANG["DESCRIPTION2"]+'<b>'+Yn+'</b>.'}`,
         chart: {
           type: 'line',
           data: {
             labels: tabMembersCount.map(item=>item.dateTime),
             datasets: [{
-              label: 'Members Count',
+              label: `${LANG["MEMBERS_COUNT"]}`,
               data: tabMembersCount.map(item=>item.membersCount),
 	      fill: false,
               backgroundColor: 'rgba(0, 0, 255, 0.5)',
@@ -120,7 +154,15 @@ module.exports = (req, res, next) => co(function *() {
               borderWidth: 1
             },
 	    {
-              label: 'Issuers Count',
+              label: `${LANG["SENTRIES_COUNT"]}`,
+              data: tabMembersCount.map(item=>item.sentriesCount),
+	      fill: false,
+              backgroundColor: 'rgba(0, 255, 0, 0.5)',
+              borderColor: 'rgba(0, 255, 0, 1)',
+              borderWidth: 1
+            },
+	    {
+              label: `${LANG["ISSUERS_COUNT"]}`,
               data: tabMembersCount.map(item=>item.issuersCount),
 	      fill: false,
               backgroundColor: 'rgba(255, 0, 0, 0.5)',
@@ -138,7 +180,7 @@ module.exports = (req, res, next) => co(function *() {
             // },
             title: {
               display: true,
-              text: ' Members Count in the range #'+begin+'-#'+end
+              text: `${LANG['CHART_TITLE']+' #'+begin+'-#'+end}`
             },
             legend: {
               display: true
