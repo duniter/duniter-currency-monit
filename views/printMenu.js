45dda67bdaf514b3a94e3aea202459af6a684612
librelois <elois@ifee.fr>
2017-07-14 16:41:01 +0200

    add printMenu function

---
+++ views/printMenu.js
@@ -0,0 +1,17 @@
+module.exports = function printMenu(lang, help) {
+  let htmlMenu = '<table align="center" width="100%"><tr>';
+
+  htmlMenu += `<td><a href="willMembers?lg=${lang['LG']}${(typeof(help) != 'undefined' && help == 'no') ? '&help=no':''}">${lang["WILL_MEMBERS"]}</a></td>`;
+  htmlMenu += `<td><a href="members?lg=${lang['LG']}${(typeof(help) != 'undefined' && help == 'no') ? '&help=no':''}">${lang["MEMBERS"]}</a></td>`;
+  htmlMenu += `<td><a href="membersCount?lg=${lang['LG']}${(typeof(help) != 'undefined' && help == 'no') ? '&help=no':''}">${lang["MEMBERS_COUNT"]}</a></td>`;
+  htmlMenu += `<td><a href="blockCount?lg=${lang['LG']}${(typeof(help) != 'undefined' && help == 'no') ? '&help=no':''}">${lang["BLOCK_COUNT"]}</a></td>`;
+  htmlMenu += `<td><a href="monetaryMass?lg=${lang['LG']}${(typeof(help) != 'undefined' && help == 'no') ? '&help=no':''}">${lang["MONETARY_MASS"]}</a></td>`;
+  htmlMenu += `<td><a href="wotex?lg=${lang['LG']}${(typeof(help) != 'undefined' && help == 'no') ? '&help=no':''}">${lang["WOTEX"]}</a></td>`;
+  htmlMenu += `<td><a href="about?lg=${lang['LG']}${(typeof(help) != 'undefined' && help == 'no') ? '&help=no':''}">${lang["ABOUT"]}</a></td>`;
+  htmlMenu += `<td><form action="" method="GET"><select name="lg" onchange="this.form.submit()">`;
+	htmlMenu += `<option name="lg" value="fr" ${lang['LG'] == 'fr' ? 'selected' : ''}>FR`;
+	htmlMenu += `<option name="lg" value="en" ${lang['LG'] == 'en' ? 'selected' : ''}>EN`;
+	htmlMenu += '</select></td></tr></table><hr>';
+        
+    return htmlMenu;
+}
