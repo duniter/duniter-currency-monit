17f9081673f15d13d5894f12f7f42f3f7e8abf18
Baptiste Lemoine <contact@cipherbliss.com>
2020-05-01 15:29:37 +0200

    [fix] remove compiled files

--- lib/MonitorExecutionTime.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function getMicrosecondsTime(): number;
-export declare function getDurationInMicroSeconds(before: number): number;
-export declare const showExecutionTimes: () => void;
-export declare const MonitorExecutionTime: (idProperty?: string) => (target: any, propertyKey: string, descriptor: PropertyDescriptor) => void;
